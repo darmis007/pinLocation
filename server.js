3f4c14203e9971fd932423b71b0358cae0964a76
darmis007 <darshmishra3010@gmail.com>
2020-06-29 17:54:27 +0530

    update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 
 const {findOrCreateUser} = require('./controllers/userController')
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser:true,useUnifiedTopology: true}).then(
+mongoose.connect("mongodb+srv://dm07:Darmis007@geopins-faaev.mongodb.net/geopins?retryWrites=true&w=majority", { useNewUrlParser:true,useUnifiedTopology: true}).then(
     () => console.log('DB connected')
 ).catch(err=> console.log(err))
 
